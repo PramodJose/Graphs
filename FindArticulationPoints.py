35f17ac9128089b410d255165d08b3409ec51ed7
Pramod Jose <pramod.gj17@gmail.com>
2018-10-29 01:58:42 +0530

    Had misunderstood the logic for identifying whether root is an Artln. Pt. or not. Fixed the logic now.

--- FindArticulationPoints.py
+++ FindArticulationPoints.py
@@ -8,12 +8,15 @@ def findArticulationPoints(graph, v):
     except AttributeError:
         findArticulationPoints.counter = 1
         graph.reset()
+        childrenOfRoot = 0
 
     v.dfsNum = v.low = findArticulationPoints.counter
     v.status = "Visited"
 
     for w in v.adjVertices:
         if w.status != "Visited":
+            if v.dfsNum == 1:           # if it is the root..
+                childrenOfRoot += 1     # .. then increment the count of children of root.
             w.parent = v
             findArticulationPoints(graph, w)
 
@@ -25,12 +28,13 @@ def findArticulationPoints(graph, v):
         elif v.parent != w:
             v.low = min(v.low, w.dfsNum)
 
-    if v.dfsNum == 1 and len(v.adjVertices) > 1:
+    if v.dfsNum == 1 and childrenOfRoot > 1:
         print(v.name, "is an articulation point")
+
 
 fin = open(INP_FILE, "r")
 NUMBER_OF_VERTICES= int(fin.readline().split()[0])
 g = header.Graph(fin, NUMBER_OF_VERTICES)
 g.displayVertices()
-findArticulationPoints(g, g.vertices[0])
+findArticulationPoints(g, g.vertices[3])
 fin.close()
